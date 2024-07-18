2e87e310be9549457912da37324ba0c714badc17
Jinjun Liu <ljj7129@gmail.com>
2024-07-18 15:09:18 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tropical-cyclone-blog.vercel.app/', // Write here your website url
+	site: 'https://blog.jinjunliu.com/', // Write here your website url
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 		drafts: true,
