3330fbb13a37161290987c43252233e1dfca8014
Jinjun Liu <ljj7129@gmail.com>
2024-07-31 03:47:26 -0500

    started homepage pagination

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -6,6 +6,7 @@ interface SiteConfig {
 	ogLocale: string
 	shareMessage: string
 	paginationSize: number
+	homePaginateSize: number
 }
 
 export const siteConfig: SiteConfig = {
@@ -15,5 +16,6 @@ export const siteConfig: SiteConfig = {
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
-	paginationSize: 6 // Number of posts per page
+	paginationSize: 3, // Number of posts per page
+	homePaginateSize: 2 // Number of posts per page on home page
 }
