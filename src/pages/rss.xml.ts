db8a0ae484ec86448118c7ffcb1a4542e2293ca5
Jinjun Liu <ljj7129@gmail.com>
2024-09-11 23:00:09 -0500

    follow challenge

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,17 +3,18 @@ import { getCollection } from 'astro:content'
 import { siteConfig } from '@/site-config'
 
 export async function GET(context: any) {
-	const posts = await getCollection('blog')
-	return rss({
-		title: siteConfig.title,
-		description: siteConfig.description,
-		site: context.site,
-		items: posts
-			.filter((post) => !post.data.draft)
-			.map((post) => ({
-				...post.data,
-				link: `post/${post.slug}/`,
-				content: post.body // This is the full Markdown content of the post
-			}))
-	})
+    const posts = await getCollection('blog')
+    return rss({
+        title: siteConfig.title,
+        // description: siteConfig.description,
+        description: 'feedId:52443064995444736+userId:46735639826583552',
+        site: context.site,
+        items: posts
+            .filter((post) => !post.data.draft)
+            .map((post) => ({
+                ...post.data,
+                link: `post/${post.slug}/`,
+                content: post.body // This is the full Markdown content of the post
+            }))
+    })
 }
