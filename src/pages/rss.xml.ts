48a7a2e9c227d6f96cdee0dab1b7679bce2dd8cb
Jinjun Liu <ljj7129@gmail.com>
2024-09-11 23:07:03 -0500

    latest post on top in the rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,17 +4,19 @@ import { siteConfig } from '@/site-config'
 
 export async function GET(context: any) {
     const posts = await getCollection('blog')
+    const sortedPosts = posts
+        .filter((post) => !post.data.draft)
+        .sort((a, b) => new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime())
+    
     return rss({
         title: siteConfig.title,
         // description: siteConfig.description,
         description: 'feedId:52443064995444736+userId:46735639826583552',
         site: context.site,
-        items: posts
-            .filter((post) => !post.data.draft)
-            .map((post) => ({
-                ...post.data,
-                link: `post/${post.slug}/`,
-                content: post.body // This is the full Markdown content of the post
-            }))
+        items: sortedPosts.map((post) => ({
+            ...post.data,
+            link: `post/${post.slug}/`,
+            content: post.body // This is the full Markdown content of the post
+        }))
     })
 }
